67fbc6638b4c616f5a1ac8f65f86c08c75f95399
bornartem <born.artem@gmail.com>
2023-08-17 17:57:04 +0300

    3 commit

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,9 @@
 public class Book {
  private String bookName;
-private String authorName;
+private Author authorName;
 private int publishingYear;
 
-    public Book (String bookName, String authorName, int publishingYear){
+    public Book (String bookName, Author authorName, int publishingYear){
         this.bookName = bookName;
         this.authorName = authorName;
         this.publishingYear = publishingYear;
@@ -11,7 +11,7 @@ private int publishingYear;
     public String getBookName(){
         return this.bookName;
     }
-    public String getAuthorName(){
+    public Author getAuthorName(){
         return this.authorName;
     }
     public int getPublishingYear(){
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-Book lordOfTheRings = new Book ("LordOfTheRings", "Tolkien", 1954);
-Author lordOfTheRings1 = new Author("John", "Tolkien");
+            Author lordOfTheRings1 = new Author("John", "Tolkien");
+Book lordOfTheRings = new Book ("LordOfTheRings", lordOfTheRings1, 1954);
         System.out.println("Название книги - " + lordOfTheRings.getBookName());
-        System.out.println("Автор книги - " + lordOfTheRings.getAuthorName());
+        System.out.println("Автор книги - " + lordOfTheRings1.getAuthorFirstName() + " " + lordOfTheRings1.getAuthorSurname());
         System.out.println("Год издания - " + lordOfTheRings.getPublishingYear());
-        System.out.println("Имя автора - " + lordOfTheRings1.getAuthorFirstName());
-        System.out.println("Фамилия автора - " + lordOfTheRings1.getAuthorSurname());
         lordOfTheRings.setPublishingYear(1950);
         System.out.println("Год издания - " + lordOfTheRings.getPublishingYear());
-        Book theWarInGaul = new Book ("Commentarii de Bello Gallic", "Caesar", 44);
-        Author Caesar = new Author("Gaius", "Caesar");
+            Author Caesar = new Author("Gaius", "Caesar");
+        Book theWarInGaul = new Book ("Commentarii de Bello Gallic", Caesar, 44);
         System.out.println("Название книги - " + theWarInGaul.getBookName());
-        System.out.println("Автор книги - " + theWarInGaul.getAuthorName());
+        System.out.println("Автор книги - " + Caesar.getAuthorFirstName()+ " " + Caesar.getAuthorSurname());
         System.out.println("Год издания - " + theWarInGaul.getPublishingYear());
-        System.out.println("Имя автора - " + Caesar.getAuthorFirstName());
-        System.out.println("Фамилия автора - " + Caesar.getAuthorSurname());
-        Book edenMartin = new Book("Eden Martin", "London", 1909);
-        Author London = new Author("Jack", "London");
+            Author London = new Author("Jack", "London");
+        Book edenMartin = new Book("Eden Martin", London, 1909);
         System.out.println("Название книги - " + edenMartin.getBookName());
-        System.out.println("Автор книги - " + edenMartin.getAuthorName());
+        System.out.println("Автор книги - " + London.getAuthorFirstName() + " " + London.getAuthorSurname());
         System.out.println("Год издания - " + edenMartin.getPublishingYear());
-        System.out.println("Имя автора - " + London.getAuthorFirstName());
-        System.out.println("Фамилия автора - " + London.getAuthorSurname());
         edenMartin.setPublishingYear(1908);
         System.out.println("Год издания - " + edenMartin.getPublishingYear());
     }
